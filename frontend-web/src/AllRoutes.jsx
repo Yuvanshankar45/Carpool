cce81bf5491d3409141dd196c0c4e93f1eda2d5d
debasist <debasis.t@gmail.com>
2023-09-16 12:44:11 +0530

    fix: signup login

---
+++ frontend-web/src/AllRoutes.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./ecom/LoginSignups/Login";
+import Signup from "./ecom/LoginSignups/Signup";
+
+// import Admin from "./Admin";
+const MainRoute = () => {
+  const Token=(child)=>{
+    let token=JSON.parse(localStorage.getItem("token"));
+    console.log(token);
+    if(token!=null){
+      return child;
+    } else {
+      alert("Login First");
+      return <Navigate to="/login" />
+    }
+  };
+  return (
+    <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
+      {/* <Route path="/admin" element={<Admin/>} /> */}
+    </Routes>
+  );
+};
+
+export default MainRoute;
